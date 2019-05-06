bae7fd2847bdec852deb1f54ae96591803c68c6b
Sarun Intaralawan <sarun.intaralawan@gmail.com>
2019-05-07 01:22:10 +0700

    feat: remove unused functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,5 +4,3 @@ import * as functions from 'firebase-functions';
 admin.initializeApp(functions.config().firebase);
 
 export { discord } from './discord';
-export { whazzup } from './whazzup';
-export { whazzupStatus } from './whazzup-status';
