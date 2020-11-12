7cf9045a58a20f728aae2c438a219fc3bfd21c0b
Sarun Intaralawan <sarun.intaralawan@ivao.aero>
2020-11-12 21:51:22 +0700

    feat: use new verification flow

--- commands/verify.ts
+++ commands/verify.ts
@@ -7,27 +7,13 @@ export const handleVerify = async (
   message: Discord.Message,
   log: Log
 ): Promise<void> => {
-  const userId = message.author.id;
-  const getKeyUrl = new URL(
-    'https://sso.th.ivao.aero/requestDiscordVerification'
-  );
-  const result = (
-    await axios.post(getKeyUrl.href, {
-      discord_id: userId,
-      apiKey: process.env['API_KEY']!
-    })
-  ).data;
-  const callbackLink = new URL('https://sso.th.ivao.aero/discord');
-  callbackLink.searchParams.set('key', result.key);
-  const loginLink = new URL('https://login.ivao.aero/index.php');
-  loginLink.searchParams.set('url', callbackLink.href);
   const dm = message.author.createDM();
   await (await dm).send(stripIndents`
     กรุณาคลิกที่ link ด้านล่างนี้ เพื่อเชื่อมต่อ IVAO Account ของคุณ กับ Discord
 
     Please click the link below to link your VID with Discord.
 
-    ${loginLink.href}`);
+    https://l.th.ivao.aero/discord-invite`);
 
   const entry = log.entry(
     undefined,
