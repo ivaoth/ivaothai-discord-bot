079a3a97b367ef8b12caa838e645dcdb19ca4e03
Sarun Intaralawan <sarun.intaralawan@ivao.aero>
2020-12-15 03:17:16 +0700

    feat: use less exhaustive method of updating guild member

--- commands/common/updateGuildMember.ts
+++ commands/common/updateGuildMember.ts
@@ -25,6 +25,8 @@ export const updateGuildMember = async (
 ): Promise<void> => {
   let suffix: string;
   let prefix: string;
+  const roles: Discord.Role[] = [];
+  let newNickname = '';
   if (userData.success) {
     if (userData.customNickname) {
       suffix = userData.customNickname;
@@ -44,94 +46,44 @@ export const updateGuildMember = async (
       const validHQStaff = positions.filter((s) => !s.includes('-'));
       if (validTHStaff.length > 0) {
         prefix = validTHStaff.join('/');
-        guildMember.roles.cache.some(
-          (r) => r.id === thailandDivisionStaffRole.id
-        ) || (await guildMember.roles.add(thailandDivisionStaffRole));
+        roles.push(thailandDivisionStaffRole);
       } else {
         prefix = userData.vid;
-        guildMember.roles.cache.some(
-          (r) => r.id === thailandDivisionStaffRole.id
-        ) && (await guildMember.roles.remove(thailandDivisionStaffRole));
       }
       if (validOtherDivisionStaff.length > 0) {
-        guildMember.roles.cache.some(
-          (r) => r.id === otherDivisionStaffRole.id
-        ) || (await guildMember.roles.add(otherDivisionStaffRole));
-      } else {
-        guildMember.roles.cache.some(
-          (r) => r.id === otherDivisionStaffRole.id
-        ) && (await guildMember.roles.remove(otherDivisionStaffRole));
+        roles.push(otherDivisionStaffRole);
       }
       if (validHQStaff.length > 0) {
-        guildMember.roles.cache.some((r) => r.id === hqStaffRole.id) ||
-          (await guildMember.roles.add(hqStaffRole));
-      } else {
-        guildMember.roles.cache.some((r) => r.id === hqStaffRole.id) &&
-          (await guildMember.roles.remove(hqStaffRole));
+        roles.push(hqStaffRole);
       }
     } else {
       prefix = userData.vid;
-      guildMember.roles.cache.some(
-        (r) => r.id === thailandDivisionStaffRole.id
-      ) && (await guildMember.roles.remove(thailandDivisionStaffRole));
-      guildMember.roles.cache.some((r) => r.id === otherDivisionStaffRole.id) &&
-        (await guildMember.roles.remove(otherDivisionStaffRole));
-      guildMember.roles.cache.some((r) => r.id === hqStaffRole.id) &&
-        (await guildMember.roles.remove(hqStaffRole));
     }
-    let newNickname = `${prefix} ${suffix}`;
+    newNickname = `${prefix} ${suffix}`;
     if (userData.division !== 'TH') {
       newNickname = newNickname.substr(0, 27) + ` - ${userData.division}`;
-      guildMember.roles.cache.some((r) => r.id === otherDivisionRole.id) ||
-        (await guildMember.roles.add(otherDivisionRole));
-      guildMember.roles.cache.some((r) => r.id === thailandDivisionRole.id) &&
-        (await guildMember.roles.remove(thailandDivisionRole));
+      roles.push(otherDivisionRole);
     } else {
-      guildMember.roles.cache.some((r) => r.id === thailandDivisionRole.id) ||
-        (await guildMember.roles.add(thailandDivisionRole));
-      guildMember.roles.cache.some((r) => r.id === otherDivisionRole.id) &&
-        (await guildMember.roles.remove(otherDivisionRole));
+      roles.push(thailandDivisionRole);
     }
-    if (newNickname !== guildMember.nickname) {
-      try {
-        await guildMember.setNickname(newNickname.substr(0, 32));
-      } catch {
-        if (notifyServerOwner) {
-          await guildMember.createDM().then((dm) => {
-            return dm.send(
-              "Well, I may be at the highest, but I can't compete with the server owner."
-            );
-          });
-        }
-      }
-    }
-    guildMember.roles.cache.some((r) => r.id === unverifiedRole.id) &&
-      (await guildMember.roles.remove(unverifiedRole));
-    guildMember.roles.cache.some((r) => r.id === verifiedRole.id) ||
-      (await guildMember.roles.add(verifiedRole));
+    roles.push(verifiedRole);
   } else {
     if (!guildMember.roles.cache.some((r) => r.id === botRole.id)) {
-      guildMember.roles.cache.some((r) => r.id === verifiedRole.id) &&
-        (await guildMember.roles.remove(verifiedRole));
-      guildMember.roles.cache.some((r) => r.id === thailandDivisionRole.id) &&
-        (await guildMember.roles.remove(thailandDivisionRole));
-      guildMember.roles.cache.some((r) => r.id === otherDivisionRole.id) &&
-        (await guildMember.roles.remove(otherDivisionRole));
-      guildMember.roles.cache.some(
-        (r) => r.id === thailandDivisionStaffRole.id
-      ) && (await guildMember.roles.remove(thailandDivisionStaffRole));
-      guildMember.roles.cache.some((r) => r.id === otherDivisionStaffRole.id) &&
-        (await guildMember.roles.remove(otherDivisionStaffRole));
-      guildMember.roles.cache.some((r) => r.id === hqStaffRole.id) &&
-        (await guildMember.roles.remove(hqStaffRole));
-      guildMember.roles.cache.some((r) => r.id === unverifiedRole.id) ||
-        (await guildMember.roles.add(unverifiedRole));
-      const nickname = `[UNVERIFIED] ${guildMember.user.username}`.substr(
-        0,
-        32
-      );
-      if (nickname !== guildMember.nickname) {
-        await guildMember.setNickname(nickname);
+      roles.push(unverifiedRole);
+      newNickname = `[UNVERIFIED] ${guildMember.user.username}`.substr(0, 32);
+    }
+  }
+  await guildMember.roles.set(roles);
+  if (newNickname !== guildMember.nickname) {
+    try {
+      await guildMember.setNickname(newNickname.substr(0, 32));
+    } catch {
+      if (notifyServerOwner) {
+        await guildMember.createDM().then((dm) => {
+          return dm.send(
+            "Well, I may be at the highest, but I can't compete with the server owner."
+          );
+        });
       }
     }
   }
